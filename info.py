ba164f4a31a13f4535697f1e23c103ada2dafd6a
Cynite <105969534+CyniteOfficial@users.noreply.github.com>
2023-06-01 10:44:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,6 +2,6 @@ API_ID       = 12124605
 API_HASH     = "5cf3577d85fd02286535ec2296934287"
 BOT_TOKEN    = "6083504683:AAEZZAePsjvcKGWOAMNTlaveNl7DD5KZLyo"
 SESSION      = "BQEVDDgAqAeqpwh6ISGOHhApFeAW2aOQ1fgvqNm5kH5dR2DFH3AGfjSAG6gtsq4Mz2Ra6uekSou5X1Breuqjs-DTnHtHNfRVDj3RB86iRFlIF1mA7R4kHDvGF-qVFGMbUjePCSFarxFZch-AHNN4SspYH_i3KHN5h_juhSOSlT5O2iVGIq2CNp22_aPxWkifsBrQnUuyJ3tKTy7_HQ734ICQ5RATZtWtMdO0kCUbY6TyhgYJ0L8jm7AnsqnEmBdM2UkF4fuuasDrOmDocrm39vbITKNgjnucAODx2AtY8bSD5qNDx-BH_vIORzuGF72wqzU19kDzlpyb_-Uqx483blPHyKfOQgAAAAFhSwkGAA"
-DATABASE_URI = "mongodb+srv://Files:Files@cluster0.vyta7vy.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://MdiskDb:MdiskDb@cluster0.lmirmtn.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001980857896
 ADMIN        = 6051691339
