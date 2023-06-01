a23cfeb780d3aee3317e1e66d590162d4909fc18
Cynite <105969534+CyniteOfficial@users.noreply.github.com>
2023-06-01 10:49:07 +0530

    Update client.py

--- client.py
+++ client.py
@@ -20,6 +20,6 @@ class Bot(Client):
         await super().start()        
         await User.start()
         Popen("python3 -m utils.delete", shell=True)       
-        print("Bot Started 🔧 Powered By @TechnicalCynite")   
+        print("Bot Started 👍🙂")   
     async def stop(self, *args):
         await super().stop()
