89ddd42f10b6154b407d362c78353d664cdaf89f
PREETMEHRA73638 <109457025+PREETMEHRA73638@users.noreply.github.com>
2023-06-05 13:05:01 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -17,7 +17,7 @@ async def search(bot, message):
     if message.text.startswith("/"):
        return    
     query   = message.text 
-    head    = "<u>Here is the results 👇\n\nPowered By </u> <b><I>@CyniteBackup</I></b>\n\n"
+    head    = "<u>Here is the results 👇\n\nPowered By </u> <b><I>@Punjabi_movies_villa</I></b>\n\n"
     results = ""
     try:
        for channel in channels:
